2b7f2abb39aec8f748b5f7dff151d544f283514d
Jrea314 <94095443+Jrea314@users.noreply.github.com>
2022-03-07 19:40:25 -0500

    Create almacenamiento.js

---
+++ componentes/producto/almacenamiento.js
@@ -0,0 +1,36 @@
+const model = require('./modelo')
+
+function agregarProducto( producto ) {
+    const objeto = new model( producto )
+    objeto.save()
+}
+
+function obtenerProductos( filtroProducto ) {
+    let filtro = {}
+    if (filtroProducto) {
+        filtro = { serie: filtroProducto }
+    }
+    const objeto = model.find( filtro )
+    return objeto
+}
+
+function actualizarProducto( producto ) {
+    const objeto = model.findOne( {serie: producto.serie} )
+    objeto.serie = producto.serie
+    objeto.nombre = producto.nombre
+    objeto.stock = producto.stock
+    objeto.valor = producto.valor
+    const resultado = objeto.save()
+    return resultado
+}
+
+function eliminarProducto( serie ) {
+    return model.deleteOne({serie: serie})
+}
+
+module.exports = {
+    agregar: agregarProducto,
+    obtener: obtenerProductos,
+    actualizar: actualizarProducto,
+    eliminar: eliminarProducto,
+}
